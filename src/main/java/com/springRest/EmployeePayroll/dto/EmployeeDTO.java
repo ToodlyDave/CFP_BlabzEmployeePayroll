bd66187d905e5ebc348b65d2aedd49bfc783d6e5
ToodlyDave <alapatdavid@gmail.com>
2022-01-24 16:58:34 +0530

    CREATE DTOs for the employee model and for the response

---
+++ src/main/java/com/springRest/EmployeePayroll/dto/EmployeeDTO.java
@@ -0,0 +1,13 @@
+package com.springRest.EmployeePayroll.dto;
+
+import java.util.Date;
+
+public class EmployeeDTO {
+
+	public String name;
+	public String gender;
+	public String department;
+	public long salary;
+	public Date date;
+	public String notes;
+}
---
+++ src/main/java/com/springRest/EmployeePayroll/dto/ResponseDTO.java
@@ -0,0 +1,9 @@
+package com.springRest.EmployeePayroll.dto;
+
+import com.springRest.EmployeePayroll.entities.Employee;
+
+public class ResponseDTO {
+
+	String message;
+	Employee data;
+}
